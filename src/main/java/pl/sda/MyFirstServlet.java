b2c09764a142ce461962daf4109c81c906087190
Piotr Zawadzki <piotr.zawadzki@tomtom.com>
2019-05-12 13:59:23 +0200

    My First servlet

---
+++ src/main/java/pl/sda/MyFirstServlet.java
@@ -0,0 +1,19 @@
+package pl.sda;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/MyFirstServlet")
+public class MyFirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello World!!!");
+    }
+}
